2661731564d050fc355b64f5d6acdf0d9a622874
Dan1elH3ctor <danielhector080@gmail.com>
2022-11-26 11:43:00 +0200

    update

--- js/code.js
+++ js/code.js
@@ -1,9 +1,21 @@
 // text field takes input of buttons
 let textInput = document.querySelector (".calculator-screen");
+let decimal = true
+function deci(numPad){
+    if (decimal==true) {
+        textInput.value = textInput.value + numPad;
+        decimal = false;
+    }
+}
+
+let toggleDecimal = ()=>{
+    decimal=true;
+}
 
 // button input to text field
 function calculations(numPad) {
     textInput.value = textInput.value + numPad;
+    decimal = true;
 }
 
 function equalResults() {
